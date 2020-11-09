2644248f36f1ccdb7483a2d1359b53e6225ff56c
Sladrigezz <Sladrigezz@github.com>
2020-11-09 18:50:59 +0300

    Error, Rooms, Home pages links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./App.css"
 
-function App(){
-  return(
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
+
+import { Route, Switch } from 'react-router-dom'
+
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  return (
     <>
-    hello
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/rooms/" component={Rooms}></Route>
+        <Route exact path="/rooms/:single-room" component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+      </Switch>
+
     </>
   )
 }
